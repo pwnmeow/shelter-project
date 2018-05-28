6a8cbeee7a81fa5b8616e97284a4ab4d9b52b07e
Sheeraz Ali <hacker.ascool@gmail.com>
2018-05-28 08:58:18 +0530

    shelter project

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+$(function(){
+    $("#compslider[data-orientation!='vertical']").twentytwenty({
+        default_offset_pct:0.2,
+        no_overlay: true,
+        move_with_handle_only: true,
+    })
+})
---
+++ js/typeme.js
@@ -0,0 +1,10 @@
+var typed = new Typed('#typeme', {
+    strings: ["First.", "In Making There Home's Beautiful",'Office \'s Professional','Business\'s Profitable','Come Work With Us'],
+    typeSpeed: 150,
+    startDelay:5,
+    backDelay:1000,
+    highlight:true,
+    loop:true,
+    backSpeed:5,
+});
+  
